f707bd83f6d88d3ae604b62f7cbd64d804a0b90b
azizbibitov11 <azizbibitov11@gmail.com>
2024-09-26 22:43:00 +0500

    added card3

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'card1.dart';
 import 'card2.dart';
+import 'card3.dart';
 
 // 1
 class Home extends StatefulWidget {
@@ -19,7 +20,7 @@ class HomeState extends State<Home> {
     // TODO: Replace with Card2
     const Card2(),
     // TODO: Replace with Card3
-    Container(color: Colors.blue)
+    const Card3(),
   ];
 
   void _onItemTapped(int index) {
